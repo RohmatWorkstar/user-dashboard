fb406ba01be386bc27469831cd4679d894ab6a79
RohmatWorkstar <rohmatworkstar99@gmail.com>
2025-02-19 12:59:11 +0700

    updated

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -4,7 +4,7 @@ const InputField = ({ label, name, register, errors, validationRules = {} }) => 
             <label className="block font-medium">{label}</label>
             <input 
                 {...register(name, validationRules)} 
-                className="p-3 border border-gray-300 rounded w-full focus:ring focus:ring-blue-200" 
+                className={`p-3 border ${errors[name] ? 'border-red-500' : 'border-gray-300'} rounded w-full focus:ring focus:ring-blue-200`} 
             />
             {errors[name] && <p className="text-red-500 text-sm mt-1">{errors[name]?.message}</p>}
         </div>
--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -2,11 +2,10 @@ import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { addUser, updateUser } from "../features/userSlice";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import InputField from "./InputField";
 
 const UserForm = () => {
-    const [formErrors, setFormErrors] = useState({});
     const {
         register,
         handleSubmit,
@@ -38,20 +37,6 @@ const UserForm = () => {
     }, [user, setValue]);
 
     const onSubmit = (data) => {
-        const errors = {};
-
-        if (!data.address?.street) {
-            errors.address = "Alamat wajib di isi";
-        }
-
-        if (!data.company?.name) {
-            errors.company = "Nama Perusahaan wajib di isi";
-        }
-
-        if (Object.keys(errors).length > 0) {
-            setFormErrors(errors);
-            return; 
-        }
         if (user) {
             dispatch(updateUser({ id: user.id, ...data }));
         } else {
@@ -64,21 +49,40 @@ const UserForm = () => {
         <div className="container mx-auto p-6">
             <h1 className="text-3xl font-bold mb-6 text-center text-white">{user ? "Edit Pengguna" : "Tambah Pengguna"}</h1>
             <form onSubmit={handleSubmit(onSubmit)} className="max-w-lg mx-auto bg-white p-6 shadow-lg rounded-lg space-y-4">
-                <InputField label="Nama" name="name" register={register} errors={errors} validationRules={{ required: "Nama Wajib di isi " }} />
+                <InputField 
+                    label="Nama" 
+                    name="name" 
+                    register={register} 
+                    errors={errors} 
+                    validationRules={{ required: "Nama Wajib di isi" }} 
+                />
                 <InputField
                     label="Email"
                     name="email"
                     register={register}
                     errors={errors}
                     validationRules={{
-                        required: "Email Wajib di isi ",
-                        pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, message: "Invalid email format" },
+                        required: "Email Wajib di isi",
+                        pattern: {
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                            message: "Format email tidak valid",
+                        },
                     }}
                 />
-                <InputField label="Alamat" name="address.street" register={register} errors={errors} validationRules={{ required: "Alamat Wajib di isi " }} />{" "}
-                {formErrors.address && <p className="text-red-500 text-sm mt-1">{formErrors.address}</p>}
-                <InputField label="Perusahaan" name="company.name" register={register} errors={errors} validationRules={{ required: "Nama Perusahaan Wajib di isi " }} />{" "}
-                {formErrors.address && <p className="text-red-500 text-sm mt-1">{formErrors.company}</p>}
+                <InputField 
+                    label="Alamat" 
+                    name="address.street" 
+                    register={register} 
+                    errors={errors} 
+                    validationRules={{ required: "Alamat Wajib di isi" }} 
+                />
+                <InputField 
+                    label="Perusahaan" 
+                    name="company.name" 
+                    register={register} 
+                    errors={errors} 
+                    validationRules={{ required: "Nama Perusahaan Wajib di isi" }} 
+                />
                 <button type="submit" className="bg-blue-600 text-white w-full py-3 rounded hover:bg-blue-700 transition">
                     Submit
                 </button>
